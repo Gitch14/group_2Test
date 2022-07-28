7c0a2296e8bccdbba31041b4e17e0d460a452de1
chicagoist <le.legioner@gmail.com>
2022-07-28 15:41:58 +0200

    src as source directory & JUnit in classpath

---
+++ src/test/java/executor/service/ApplicationTest.java
@@ -0,0 +1,20 @@
+package executor.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class ApplicationTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
