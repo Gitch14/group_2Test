23e8f116378a933505743d079c5e473b17aa9ffc
Helen_Kerenykevych <helenchekanovska@gmail.com>
2022-07-29 15:28:59 +0300

    feat: add DTO Step and StepTest

---
+++ src/main/java/executor/service/model/Step.java
@@ -0,0 +1,13 @@
+package executor.service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Step {
+    String action;
+    String value;
+}
