d01910ef9acd67302ac94ffc85acacc38a7c0e67
G1tch14 <bukachvladislav@gmail.com>
2022-07-30 00:01:25 +0300

    -lombok

--- src/main/java/executor/service/model/WebDriverConfig.java
+++ src/main/java/executor/service/model/WebDriverConfig.java
@@ -1,22 +1,56 @@
 package executor.service.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.Objects;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class WebDriverConfig {
 
     private String webDriverExecutable;
     private String userAgent;
     private Long pageLoadTimeout;
     private Long implicitlyWait;
+
+ public WebDriverConfig(String webDriverExecutable, String userAgent, Long pageLoadTimeout, Long implicitlyWait) {
+        this.webDriverExecutable = webDriverExecutable;
+        this.userAgent = userAgent;
+        this.pageLoadTimeout = pageLoadTimeout;
+        this.implicitlyWait = implicitlyWait;
+    }
+
+    public WebDriverConfig() {
+    }
+
+    public String getWebDriverExecutable() {
+        return webDriverExecutable;
+    }
+
+    public void setWebDriverExecutable(String webDriverExecutable) {
+        this.webDriverExecutable = webDriverExecutable;
+    }
+
+    public String getUserAgent() {
+        return userAgent;
+    }
+
+    public void setUserAgent(String userAgent) {
+        this.userAgent = userAgent;
+    }
+
+    public Long getPageLoadTimeout() {
+        return pageLoadTimeout;
+    }
+
+    public void setPageLoadTimeout(Long pageLoadTimeout) {
+        this.pageLoadTimeout = pageLoadTimeout;
+    }
+
+    public Long getImplicitlyWait() {
+        return implicitlyWait;
+    }
+
+    public void setImplicitlyWait(Long implicitlyWait) {
+        this.implicitlyWait = implicitlyWait;
+    }
 
 
     @Override
---
+++ src/test/java/executor/service/model/ThreadPoolConfigTest.java
@@ -0,0 +1,21 @@
+package executor.service.model;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ThreadPoolConfigTest {
+
+@Test
+    public void ThreadPoolConfigTest(){
+         ThreadPoolConfig th1 = new ThreadPoolConfig(10, 1_000_000_000L);
+         ThreadPoolConfig th2 = new ThreadPoolConfig(10, 1_000_000_000L);
+        assertEquals(th1, th2);
+        th1.setCorePoolSize(th1.getCorePoolSize()*2);
+        th1.setKeepAliveTime(th1.getKeepAliveTime()*2);
+        ThreadPoolConfig actual = new ThreadPoolConfig(20, 2_000_000_000L);
+        assertEquals(actual,th1);
+    }
+
+}
