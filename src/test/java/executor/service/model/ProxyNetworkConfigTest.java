44b29c88b060ad3810e23b0bd066d7027748291c
N1k0l4ss <67698184+N1k0l4ss@users.noreply.github.com>
2022-07-31 15:09:44 +0300

    Create WebDriverConfig DTO and Test

---
+++ src/test/java/executor/service/model/ProxyNetworkConfigTest.java
@@ -0,0 +1,29 @@
+package executor.service.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProxyNetworkConfigTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getHostname() {
+    }
+
+    @Test
+    void setHostname() {
+    }
+
+    @Test
+    void getPort() {
+    }
+
+    @Test
+    void setPort() {
+    }
+}
