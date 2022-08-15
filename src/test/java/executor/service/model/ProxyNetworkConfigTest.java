509d26dbcf1181e4a24b78da1d43cdd5f0c19071
N1k0l4ss <nikita777white@gmail.com>
2022-08-15 10:10:47 +0300

    ProxyNetworkConfig DTO+Test fixes

--- src/test/java/executor/service/model/ProxyNetworkConfigTest.java
+++ src/test/java/executor/service/model/ProxyNetworkConfigTest.java
@@ -7,23 +7,4 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ProxyNetworkConfigTest {
 
-    @BeforeEach
-    void setUp() {
-    }
-
-    @Test
-    void getHostname() {
-    }
-
-    @Test
-    void setHostname() {
-    }
-
-    @Test
-    void getPort() {
-    }
-
-    @Test
-    void setPort() {
-    }
 }
