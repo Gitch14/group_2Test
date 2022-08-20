d14e0daf896676ceddbc6e4c2a0d8ebcd15f6ffe
G1tch14 <bukachvladislav@gmail.com>
2022-08-20 23:12:57 +0300

    r

--- src/test/java/executor/service/StepExecutionServiceClickCssTest.java
+++ src/test/java/executor/service/StepExecutionServiceClickCssTest.java
@@ -2,23 +2,13 @@ package executor.service;
 
 import executor.service.impl.WebDriverChromeService;
 
-import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class StepExecutionServiceClickCssTest {
-
-   @BeforeClass
-    public static void setup() {
-
-       System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver");
-   }
-
     @Test
     public void ClickCssTest(){
-
-
         WebDriverInitializerService browser = new WebDriverChromeService();
         WebDriver chrome = browser.create();
 
