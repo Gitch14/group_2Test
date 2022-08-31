e9e3de6a73f540e0e6bb3feb950d83f1a9dd6cf9
N1k0l4ss <nikita777white@gmail.com>
2022-08-31 10:27:36 +0300

    ScenarioSourceListenerService created, add maven jackson-databind dependency, add test for ScenarioSourceListenerService

---
+++ src/test/java/executor/service/impl/ScenarioSourceListenerServiceTest.java
@@ -0,0 +1,26 @@
+package executor.service.impl;
+
+import executor.model.Scenario;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ScenarioSourceListenerServiceTest {
+
+    private final ScenarioSourceListenerService scenarioSourceListener = new ScenarioSourceListenerService();
+    private List<Scenario> scenarios;
+
+    @BeforeEach
+    void setUp() {
+        scenarioSourceListener.execute();
+        scenarios = scenarioSourceListener.getScenarios();
+    }
+
+    @Test
+    void execute() {
+        assertEquals(scenarios.get(1).getName(), "test scenario 2", "JSON mapper work incorrectly");
+    }
+}
