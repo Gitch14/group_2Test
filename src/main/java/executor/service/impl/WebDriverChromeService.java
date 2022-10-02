9edd9f7e682b905500008e097230844f7e2244f4
Denis_Lapshin <denislapshin1@gmail.com>
2022-10-02 18:42:36 +0300

    resolve conflicts

--- src/main/java/executor/service/impl/WebDriverChromeService.java
+++ src/main/java/executor/service/impl/WebDriverChromeService.java
@@ -1,22 +1,38 @@
 package executor.service.impl;
 
-import executor.util.Property;
 import executor.service.WebDriverInitializerService;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WebDriverChromeService implements WebDriverInitializerService {
 
-    private Property property;
+    @Value("${ProxyConfigHolder.ProxyCredentials.username}")
+    private String username;
 
-    @Autowired
-    public WebDriverChromeService(Property property) {
-        this.property = property;
-    }
+    @Value("${ProxyConfigHolder.ProxyCredentials.password}")
+    private String password;
+
+    @Value("${ProxyConfigHolder.ProxyNetworkConfig.hostname}")
+    private String hostname;
+
+    @Value("${ProxyConfigHolder.ProxyNetworkConfig.port}")
+    private String port;
+
+    @Value("${WebDriverConfig.webDriverExecutable}")
+    private String webDriverExecutable;
+
+    @Value("${WebDriverConfig.userAgent}")
+    private String userAgent;
+
+    @Value("${WebDriverConfig.pageLoadTimeout}")
+    private String pageLoadTimeout;
+
+    @Value("${WebDriverConfig.implicitlyWait}")
+    private String implicitlyWait;
 
     public WebDriverChromeService() {
 
@@ -32,10 +48,10 @@ public class WebDriverChromeService implements WebDriverInitializerService {
 
     @Override
     public WebDriver create() {
-        property.connect();
         ChromeOptions options = new ChromeOptions();
 
-        options.addArguments(property.connect());
+        options.addArguments(username, password, hostname, port,
+                webDriverExecutable, userAgent, pageLoadTimeout, implicitlyWait);
         return new ChromeDriver(options);
     }
 }
