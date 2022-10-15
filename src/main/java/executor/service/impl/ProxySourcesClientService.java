4d078d199ec1a4fc7351234acbcc853084dc2edb
Denis_Lapshin <denislapshin1@gmail.com>
2022-10-15 18:55:31 +0300

    updated /resources paths

--- src/main/java/executor/service/impl/ProxySourcesClientService.java
+++ src/main/java/executor/service/impl/ProxySourcesClientService.java
@@ -8,50 +8,51 @@ import executor.service.ProxySourcesClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 @Service
-public class ProxySourcesClientService implements ProxySourcesClient {
-
+public class ProxySourcesClientService implements ProxySourcesClient
+{
+    
     private ObjectMapper objectMapper;
-
+    
     @Autowired
-    public ProxySourcesClientService(ObjectMapper objectMapper) {
+    public ProxySourcesClientService(ObjectMapper objectMapper)
+    {
         this.objectMapper = objectMapper;
     }
-
-    public ProxySourcesClientService() {
-
+    
+    public ProxySourcesClientService()
+    {
+    
     }
-
+    
     @Override
-    public ProxyConfigHolder getProxy() {
+    public ProxyConfigHolder getProxy()
+    {
         List<ProxyCredentials> credentials = new ArrayList<>();
         List<ProxyNetworkConfig> network = new ArrayList<>();
-        try {
-            URL credentialURI = this.getClass().getClassLoader().getResource("ProxyCredentials.json");
-            URL networkURI = this.getClass().getClassLoader().getResource("ProxyNetwork.json");
-
-            if (credentialURI != null && networkURI != null) {
-                File file = new File(credentialURI.toURI());
-                credentials.addAll(List.of(objectMapper.readValue(file, ProxyCredentials[].class)));
-                file = new File(networkURI.toURI());
-                network.addAll(List.of(objectMapper.readValue(file, ProxyNetworkConfig[].class)));
-            }
-        } catch (URISyntaxException | IOException e) {
-            e.printStackTrace();
+        
+        try (InputStream proxyCredentialsInputStream = getClass().getResourceAsStream("/ProxyCredentials.json");
+             InputStream proxyNetworkInputStream = getClass().getResourceAsStream("/ProxyNetwork.json");
+             BufferedReader proxyCredentialsReader = new BufferedReader(new InputStreamReader(proxyCredentialsInputStream));
+             BufferedReader proxyNetworkReader = new BufferedReader(new InputStreamReader(proxyNetworkInputStream)))
+        {
+            credentials.addAll(List.of(objectMapper.readValue(proxyCredentialsReader, ProxyCredentials[].class)));
+            network.addAll(List.of(objectMapper.readValue(proxyNetworkReader, ProxyNetworkConfig[].class)));
         }
-
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+        
         Random random = new Random();
         int randomNetwork = random.nextInt(network.size());
         int randomCredential = random.nextInt(credentials.size());
-
+        
         return new ProxyConfigHolder(network.get(randomNetwork), credentials.get(randomCredential));
     }
 }
